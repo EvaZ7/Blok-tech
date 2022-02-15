164aee878c910bc799196be7cb3f15e5fd70a3b1
EvaZ7 <evazaadnoordijk@gmail.com>
2022-02-15 15:10:07 +0100

    static

--- index.js
+++ index.js
@@ -1,9 +1,21 @@
 const express = require('express')
 const app = express()
-const port = 3000
+const port = process.env.prt || 1337
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
+})
+
+app.get('/about', (req, res) => {
+  res.send('Over ons')
+})
+
+app.get('/info', (req, res) => {
+  res.send('Meer info')
+})
+
+app.get('*', (req, res) => {
+  res.send('Not found')
 })
 
 app.listen(port, () => {
