e93f31e9b3019bf32e2027264ca33bfa95bc556a
EvaZ7 <evazaadnoordijk@gmail.com>
2022-03-04 20:19:43 +0100

    scss

--- index.js
+++ index.js
@@ -1,26 +1,28 @@
 const express = require('express')
 const {engine} = require('express-handlebars')
 //const req = require("express/lib/request")
-
 const bodyParser = require('body-parser')
-
 //const slug = require('slug')
-
+//afbeeldingen uploaden en handelen
 const multer  = require('multer')
 const upload = multer({ dest: 'uploads/'})
 const app = express();
-
-//const filepath = '~/Blok-tech/static/media/404giphy.gif'
-
-//require('dotenv').config()
-
+//installatie sass
+const sass = require('sass');
+const result = sass.compile("style.css");
+console.log(result.css);
+//const filepath = '~/Blok-tech/static/media/404giphy.gif' ---was te proberen
+//init enviromental variables
+require('dotenv').config()
+//set poort 1337
 const PORT = 1337;
-
+//initialize handlebars
 app.engine('handlebars', engine());
-
+//set view engine to handlebars
 app.set('view engine', 'handlebars');
-
+//set view directory equal to views
 app.set('views', './views')
+//home route where each request that is the root responds to
 
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
