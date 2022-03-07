72facd85e1ccefbd757327b42ae571aa07a57860
EvaZ7 <evazaadnoordijk@gmail.com>
2022-03-07 15:58:45 +0100

    fix

--- app.js
+++ app.js
@@ -30,7 +30,7 @@ app.use(bodyParser.json());
 app.use(express.static('static'));
 
 app.get('/', (req, res) => { //home invoegen
-  res.render('main');
+  res.render('getstarted');
 });
 
 //app.get('/about', (req, res) => {
